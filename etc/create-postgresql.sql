c1683091751f03360d50b474e87d4edeef938f1d
Johannes Beck <mail@johannes-beck.name>
2013-12-20 20:29:00 +0100

    Add jsr-356 library

---
+++ etc/create-postgresql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE stocks.share (
+  id bigint NOT NULL,
+  key character varying(25) NOT NULL,
+  name character varying(80) NOT NULL,
+  version bigint,
+  CONSTRAINT share_pkey PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX idx_key ON stocks.share (key);
+CREATE INDEX idx_name ON stocks.share (name);
+CREATE SEQUENCE stocks.hibernate_sequence;
