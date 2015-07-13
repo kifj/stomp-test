98ad688fdc684df6a9ff24a79f4cf20b35bde2d4
Johannes Beck <mail@johannes-beck.name>
2015-07-13 20:22:00 +0200

    Add integration-tests as docker build

--- etc/docker/integration-tests/scripts/configure.sh
+++ etc/docker/integration-tests/scripts/configure.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-MGMT_USER=admin
-MGMT_PASSWORD=SandTHEM
-
 export JBOSS_BASE_DIR=$1
 export JAVA_HOME=/usr/java/default
 export JBOSS_HOME=/opt/wildfly-9.0.0.Final
@@ -9,7 +6,6 @@ export RUN_CONF=$WILDFLY_DIR/standalone.conf
 export MAIL_SERVER=localhost
 export MANAGEMENT=local
 
-$JBOSS_HOME/bin/add-user.sh -s  -u $MGMT_USER -r ManagementRealm -sc $JBOSS_BASE_DIR/configuration -p $MGMT_PASSWORD
 $JBOSS_HOME/bin/add-user.sh -s  -a -u $MGMT_USER -g admin,guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p $MGMT_PASSWORD
 $JBOSS_HOME/bin/add-user.sh -s  -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p guest_12345!
 
@@ -21,6 +17,5 @@ $JBOSS_HOME/bin/jboss-cli.sh --connect --controller=localhost:9990 --file=$JBOSS
 kill -HUP $(pidof java)
 sleep 3
 rm slot0/log/*.log
-
 
 exit 0
