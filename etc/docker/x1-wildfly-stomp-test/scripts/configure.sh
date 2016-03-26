e0f74d303550fa8a2e6bf9a3d8761a3e1c079f71
Johannes Beck <mail@johannes-beck.name>
2016-03-26 14:53:38 +0100

    Enable statistics for DS

--- etc/docker/x1-wildfly-stomp-test/scripts/configure.sh
+++ etc/docker/x1-wildfly-stomp-test/scripts/configure.sh
@@ -13,7 +13,7 @@ $JBOSS_HOME/bin/add-user.sh -s  -u $MGMT_USER -r ManagementRealm -sc $JBOSS_BASE
 $JBOSS_HOME/bin/add-user.sh -s  -a -u $MGMT_USER -g admin,guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p $MGMT_PASSWORD
 $JBOSS_HOME/bin/add-user.sh -s  -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p guest_12345!
 
-$JBOSS_HOME/bin/standalone.sh -c profile-slot0.xml &
+$JBOSS_HOME/bin/standalone.sh -c profile-slot0.xml --admin-only &
 sleep 20
 
 $JBOSS_HOME/bin/jboss-cli.sh --connect --controller=localhost:9990 --file=$JBOSS_BASE_DIR/../scripts/create-stomp-test.cli
