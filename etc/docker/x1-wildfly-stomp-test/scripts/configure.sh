5d6d1fc60adc3363d7dd40110a7f68d970167e90
Johannes Beck <mail@johannes-beck.name>
2016-04-23 21:37:47 +0200

    Update docker configuration

--- etc/docker/x1-wildfly-stomp-test/scripts/configure.sh
+++ etc/docker/x1-wildfly-stomp-test/scripts/configure.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 MGMT_USER=admin
-MGMT_PASSWORD=SandTHEM
+MGMT_PASSWORD=12345
 
 export JBOSS_BASE_DIR=$1
 export JAVA_HOME=/usr/java/default
-export JBOSS_HOME=/opt/wildfly-10.0.0.Final
+export JBOSS_HOME=/opt/wildfly
 export RUN_CONF=$WILDFLY_DIR/standalone.conf
 export MAIL_SERVER=localhost
 export MANAGEMENT=local
 
-$JBOSS_HOME/bin/add-user.sh -s  -u $MGMT_USER -r ManagementRealm -sc $JBOSS_BASE_DIR/configuration -p $MGMT_PASSWORD
-$JBOSS_HOME/bin/add-user.sh -s  -a -u $MGMT_USER -g admin,guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p $MGMT_PASSWORD
-$JBOSS_HOME/bin/add-user.sh -s  -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p guest_12345!
+$JBOSS_HOME/bin/add-user.sh -s -u $MGMT_USER -r ManagementRealm -sc $JBOSS_BASE_DIR/configuration -p $MGMT_PASSWORD
+$JBOSS_HOME/bin/add-user.sh -s -a -u $MGMT_USER -g admin,guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p $MGMT_PASSWORD
+$JBOSS_HOME/bin/add-user.sh -s -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p guest_12345!
 
 $JBOSS_HOME/bin/standalone.sh -c profile-slot0.xml --admin-only &
 sleep 20
@@ -21,6 +21,5 @@ $JBOSS_HOME/bin/jboss-cli.sh --connect --controller=localhost:9990 --file=$JBOSS
 kill -HUP $(pidof java)
 sleep 3
 rm slot0/log/*.log
-
 
 exit 0
