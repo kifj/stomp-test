b6933caa578c5b739269b148bd5100927d2b27a1
Johannes Beck <mail@johannes-beck.name>
2017-06-25 12:51:47 +0200

    Update docker logging

--- src/main/docker/integration-tests/scripts/configure.sh
+++ src/main/docker/integration-tests/scripts/configure.sh
@@ -16,7 +16,7 @@ $JBOSS_HOME/bin/add-user.sh -s -a -u guest -g guest -r ApplicationRealm -sc $JBO
 /usr/local/bin/create-keystore.sh $JBOSS_BASE_DIR/configuration jboss12345 jboss.jboss
 
 $JBOSS_HOME/bin/standalone.sh -c profile-slot0.xml --admin-only &
-sleep 10
+sleep 20
 
 $JBOSS_HOME/bin/jboss-cli.sh --connect --controller=localhost:9990 --file=$JBOSS_BASE_DIR/../scripts/create-stomp-test.cli
 $JBOSS_HOME/bin/jboss-cli.sh --connect --controller=localhost:9990 --command=:shutdown
