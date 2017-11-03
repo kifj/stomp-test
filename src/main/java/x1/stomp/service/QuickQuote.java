874ee7705d81e56a7aa09a2a52be3726fdd54691
Johannes Beck <mail@johannes-beck.name>
2017-11-03 19:36:34 +0100

    Replace quote service from Yahoo, since it is no longer available

---
+++ src/main/java/x1/stomp/service/QuickQuote.java
@@ -0,0 +1,79 @@
+package x1.stomp.service;
+
+import java.io.Serializable;
+
+public class QuickQuote implements Serializable {
+  private static final long serialVersionUID = -7248251946071933412L;
+
+  private Float last;
+  private String name;
+  private String symbol;
+  private Integer volume;
+  private String countryCode;
+  private String currencyCode;
+  private String exchange;
+
+  public Float getLast() {
+    return last;
+  }
+
+  public void setLast(Float last) {
+    this.last = last;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSymbol() {
+    return symbol;
+  }
+
+  public void setSymbol(String symbol) {
+    this.symbol = symbol;
+  }
+
+  public Integer getVolume() {
+    return volume;
+  }
+
+  public void setVolume(Integer volume) {
+    this.volume = volume;
+  }
+
+  public String getCountryCode() {
+    return countryCode;
+  }
+
+  public void setCountryCode(String countryCode) {
+    this.countryCode = countryCode;
+  }
+
+  public String getCurrencyCode() {
+    return currencyCode;
+  }
+
+  public void setCurrencyCode(String currencyCode) {
+    this.currencyCode = currencyCode;
+  }
+
+  public String getExchange() {
+    return exchange;
+  }
+
+  public void setExchange(String exchange) {
+    this.exchange = exchange;
+  }
+
+
+  @Override
+  public String toString() {
+    return "QuickQuote [last=" + last + ", name=" + name + ", symbol=" + symbol + ", volume=" + volume
+        + ", countryCode=" + countryCode + ", currencyCode=" + currencyCode + ", exchange=" + exchange + "]";
+  }
+
+}
--- src/test/java/x1/stomp/test/ShareSubscriptionTest.java
+++ src/test/java/x1/stomp/test/ShareSubscriptionTest.java
@@ -24,75 +24,66 @@ import org.slf4j.Logger;
 
 @RunWith(Arquillian.class)
 public class ShareSubscriptionTest {
-	@Deployment
-	public static Archive<?> createTestArchive() {
-		File[] libraries = Maven
-				.resolver()
-				.loadPomFromFile("pom.xml")
-				.resolve(
-				    "org.apache.httpcomponents:fluent-hc", 
-				    "org.apache.commons:commons-lang3",
-						"io.swagger:swagger-jaxrs")
-				.withTransitivity()
-				.asFile();
-		return ShrinkWrap.create(WebArchive.class, "stomp-test.war")
-		    .addPackages(true, "x1.stomp")
-				.addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
-				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-				.addAsWebInfResource("test-ds.xml")
-				.addAsWebInfResource("jboss-deployment-structure.xml")
-				.addAsLibraries(libraries);
-	}
+  @Deployment
+  public static Archive<?> createTestArchive() {
+    File[] libraries = Maven.resolver().loadPomFromFile("pom.xml")
+        .resolve("org.apache.httpcomponents:fluent-hc", "org.apache.commons:commons-lang3", "io.swagger:swagger-jaxrs")
+        .withTransitivity().asFile();
+    return ShrinkWrap.create(WebArchive.class, "stomp-test.war").addPackages(true, "x1.stomp")
+        .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+        .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml").addAsWebInfResource("test-ds.xml")
+        .addAsWebInfResource("jboss-deployment-structure.xml").addAsLibraries(libraries);
+  }
 
-	@Inject
-	private ShareSubscription shareSubscription;
+  @Inject
+  private ShareSubscription shareSubscription;
 
-	@Inject
-	private QuoteUpdater quoteUpdater;
+  @Inject
+  private QuoteUpdater quoteUpdater;
 
-	@Inject
-	private Logger log;
+  @Inject
+  private Logger log;
 
-	@Test
-	public void testSubscribe() throws Exception {
+  @Test
+  public void testSubscribe() throws Exception {
     Share share = new Share();
     share.setKey("MSFT");
     share.setName("Microsoft Corpora");
-    
-		while (true) {
-			Share existing = shareSubscription.find(share.getKey());
-			if (existing != null) {
-				shareSubscription.unsubscribe(existing);
-			} else {
-				break;
-			}
-		}
-	  
-		shareSubscription.subscribe(share);
-		assertNotNull(share.getId());
+
+    while (true) {
+      Share existing = shareSubscription.find(share.getKey());
+      if (existing != null) {
+        shareSubscription.unsubscribe(existing);
+      } else {
+        break;
+      }
+    }
+
+    shareSubscription.subscribe(share);
+    assertNotNull(share.getId());
     assertNotNull(share.getVersion());
-		log.info("{} was persisted with id {}", share.getName(), share.getId());
-		//nothing happens 
+    log.info("{} was persisted with id {}", share.getName(), share.getId());
+    // nothing happens
     shareSubscription.subscribe(share);
 
-		share = shareSubscription.find(share.getKey());
-		assertNotNull(share);
-		assertEquals(1, shareSubscription.list().size());
-
-		shareSubscription.unsubscribe(share);
-		assertNull(shareSubscription.find(share.getKey()));
-	}
-
-	@Test
-	public void testQuoteUpdater() throws Exception {
-		Share share = new Share();
-		share.setKey("GOOG");
-		share.setName("Google");
-		shareSubscription.subscribe(share);
-		quoteUpdater.updateQuotes();
-		assertEquals(1, quoteUpdater.getLastUpdateCount());
-		Thread.sleep(3000);
     share = shareSubscription.find(share.getKey());
-		shareSubscription.unsubscribe(share);
-	}
+    assertNotNull(share);
+    assertEquals(1, shareSubscription.list().size());
+
+    shareSubscription.unsubscribe(share);
+    assertNull(shareSubscription.find(share.getKey()));
+  }
+
+  @Test
+  public void testQuoteUpdater() throws Exception {
+    Share share = new Share();
+    share.setKey("GOOG");
+    share.setName("Google");
+    shareSubscription.subscribe(share);
+    quoteUpdater.updateQuotes();
+    assertEquals(1, quoteUpdater.getLastUpdateCount());
+    Thread.sleep(3000);
+    share = shareSubscription.find(share.getKey());
+    shareSubscription.unsubscribe(share);
+  }
 }
