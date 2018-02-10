2f0b3a006777f98bc6d684aac6326483b2e7631a
Johannes Beck <mail@johannes-beck.name>
2018-02-10 15:19:33 +0100

    Write dates in JSON

--- src/main/java/x1/stomp/util/JsonHelper.java
+++ src/main/java/x1/stomp/util/JsonHelper.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 import org.apache.commons.lang3.StringUtils;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -15,7 +17,8 @@ public class JsonHelper {
       return null;
     }
     ObjectMapper mapper = new ObjectMapper();
-    mapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).enable(SerializationFeature.WRAP_ROOT_VALUE);
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).enable(SerializationFeature.WRAP_ROOT_VALUE)
+        .setSerializationInclusion(Include.NON_NULL);
     StringWriter sw = new StringWriter();
     mapper.writeValue(sw, obj);
     return sw.toString();
