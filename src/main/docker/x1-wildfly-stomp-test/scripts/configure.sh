e4fa3a7b43a80bd33474daf8b135691b1975bd25
Johannes Beck <mail@johannes-beck.name>
2018-06-02 16:28:58 +0200

    Fix docker build scripts

--- src/main/docker/x1-wildfly-stomp-test/scripts/configure.sh
+++ src/main/docker/x1-wildfly-stomp-test/scripts/configure.sh
@@ -6,15 +6,15 @@ export JBOSS_HOME=/opt/wildfly
 export RUN_CONF=$JBOSS_BASE_DIR/standalone.conf
 export JAVA_HOME=/usr/java/default
 
-$JBOSS_HOME/bin/add-user.sh -s -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p guest_12345!
+su jboss -c "$JBOSS_HOME/bin/add-user.sh -s -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p guest_12345!"
 
 /usr/local/bin/create-keystore.sh $JBOSS_BASE_DIR/configuration jboss12345 jboss.jboss
 
-$JBOSS_HOME/bin/standalone.sh -c profile-slot0.xml --admin-only >& /dev/null &
+su jboss -c "$JBOSS_HOME/bin/standalone.sh -c profile-slot0.xml --admin-only" &
 timeout 30 bash -c 'until echo > /dev/tcp/localhost/9990; do sleep 1; done' >& /dev/null || exit -1
 
-$JBOSS_HOME/bin/jboss-cli.sh --connect --controller=localhost:9990 --file=$JBOSS_BASE_DIR/../scripts/create-stomp-test.cli
-$JBOSS_HOME/bin/jboss-cli.sh --connect --controller=localhost:9990 --command=:shutdown
+su jboss -c "$JBOSS_HOME/bin/jboss-cli.sh --connect --controller=localhost:9990 --file=$JBOSS_BASE_DIR/../scripts/create-stomp-test.cli"
+su jboss -c "$JBOSS_HOME/bin/jboss-cli.sh --connect --controller=localhost:9990 --command=:shutdown"
 
 rm $JBOSS_BASE_DIR/log/*.log $JBOSS_BASE_DIR/configuration/keystore.jks
 exit 0
