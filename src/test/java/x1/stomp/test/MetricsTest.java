03d77150e91b4dc6297fcd1c82a0559438226ee6
Johannes Beck <mail@johannes-beck.name>
2019-09-30 20:09:31 +0200

    Add tests for Metrics and Health check

---
+++ src/test/java/x1/stomp/test/MetricsTest.java
@@ -0,0 +1,115 @@
+package x1.stomp.test;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.assertj.core.api.Assertions.assertThat;
+import static x1.stomp.test.ResponseAssert.assertThat;
+
+import java.net.URL;
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import static javax.ws.rs.core.Response.Status.*;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.gson.JsonParser;
+
+import x1.stomp.boundary.JacksonConfig;
+import x1.stomp.model.Share;
+import x1.stomp.util.VersionData;
+
+@RunWith(Arquillian.class)
+public class MetricsTest {
+  private static final String PATH_SHARES = "shares";
+  
+  private String baseUrl;
+  private String metricsBaseUrl;
+  private Client client;
+
+  @Deployment
+  public static Archive<?> createTestArchive() {
+    var libraries = Maven.resolver().loadPomFromFile("pom.xml")
+        .resolve("io.swagger.core.v3:swagger-jaxrs2", "org.assertj:assertj-core").withTransitivity().asFile();
+
+    return ShrinkWrap.create(WebArchive.class, VersionData.APP_NAME_MAJOR_MINOR + ".war").addPackages(true, "x1.stomp")
+        .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+        .addAsResource("microprofile-config.properties", "META-INF/microprofile-config.properties")
+        .addAsWebInfResource("beans.xml").addAsWebInfResource("test-ds.xml")
+        .addAsWebInfResource("jboss-deployment-structure.xml").addAsLibraries(libraries);
+  }
+
+  @ArquillianResource
+  private URL url;
+  
+  @Before
+  public void setup() {
+    client = ClientBuilder.newClient().register(JacksonConfig.class);
+    baseUrl = url.toString() + "rest";
+    metricsBaseUrl = getBaseUrlForMetrics();
+  }
+
+  @After
+  public void teardown() {
+    client.close();
+  }
+  
+  protected String getBaseUrlForMetrics() {
+    var host = getHost();
+    var port = 9990 + getPortOffset();
+    return "http://" + host + ":" + port;
+  }
+  
+  protected Integer getPortOffset() {
+    return Integer.valueOf(System.getProperty("jboss.socket.binding.port-offset", "0"));
+  }
+
+  protected String getHost() {
+    return System.getProperty("jboss.bind.address", "127.0.0.1");
+  }
+
+  @Test
+  public void testMetrics() {
+    var shares = client.target(baseUrl).path(PATH_SHARES).request(APPLICATION_JSON).get(new GenericType<List<Share>>() {
+    });
+    assertThat(shares).isEmpty();
+
+    var response = client.target(metricsBaseUrl).path("metrics").path("application").request(APPLICATION_JSON)
+        .get();
+    assertThat(response).hasStatus(OK);
+    
+    var body = response.readEntity(String.class);
+    assertThat(body).isNotNull();
+    
+    var o = new JsonParser().parse(body).getAsJsonObject();
+    assertThat(o.getAsJsonObject("add-share-timer").getAsJsonPrimitive("count").getAsInt()).isEqualTo(0);
+    assertThat(o.getAsJsonObject("get-share-timer").getAsJsonPrimitive("count").getAsInt()).isEqualTo(0);
+    assertThat(o.getAsJsonObject("remove-share-timer").getAsJsonPrimitive("count").getAsInt()).isEqualTo(0);
+    assertThat(o.getAsJsonObject("get-shares-timer").getAsJsonPrimitive("count").getAsInt()).isEqualTo(1);
+  }
+
+  @Test
+  public void testHealth() {
+    var response = client.target(metricsBaseUrl).path("health").request(APPLICATION_JSON).get();
+    assertThat(response).hasStatus(OK);
+    
+    var body = response.readEntity(String.class);
+    assertThat(body).isNotNull();
+    
+    var o = new JsonParser().parse(body).getAsJsonObject();
+    assertThat(o.get("outcome").getAsString()).isEqualTo("UP");
+    var checks = o.getAsJsonArray("checks");
+    assertThat(checks).hasSize(3);
+  }
+}
