df04bf89c9d15234768582ab5cdaaeb69aa2a5e6
Johannes Beck <mail@johannes-beck.name>
2020-02-08 11:37:01 +0100

    Disable ssl

--- src/main/docker/x1-postgres-stomp-test/create-db.sh
+++ src/main/docker/x1-postgres-stomp-test/create-db.sh
@@ -2,7 +2,7 @@
 DB_NAME=$1
 WD=$(dirname $0)
 
-/usr/bin/postgres -D /var/lib/pgsql/data -p 5432 &
+/usr/bin/postgres -D /var/lib/pgsql/data -p 5432 -c "ssl=off" &
 sleep 10
 createdb --locale=en_US.UTF-8 $DB_NAME || exit 1
 psql -d $DB_NAME -f $WD/sql/ddl.sql  || exit 1
