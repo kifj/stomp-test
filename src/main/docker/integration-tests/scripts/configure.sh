d33378354c1a2f9526fac6aeb4af3a9c07e8188c
Johannes Beck <mail@johannes-beck.name>
2020-05-02 09:56:32 +0200

    Update to new directory layout in docker

--- src/main/docker/integration-tests/scripts/configure.sh
+++ src/main/docker/integration-tests/scripts/configure.sh
@@ -7,7 +7,7 @@ export JBOSS_BASE_DIR=$1
 export JBOSS_HOME=/opt/wildfly
 export JAVA_OPTS="-Djboss.server.base.dir=$JBOSS_BASE_DIR"
 
-chown -R jboss.jboss /srv/wildfly
+chown -R jboss.jboss "$JBOSS_BASE_DIR"
 cd "$JBOSS_BASE_DIR"
 
 su jboss -c "$JBOSS_HOME/bin/add-user.sh -s -u $MGMT_USER -r ManagementRealm -sc $JBOSS_BASE_DIR/configuration -p $MGMT_PASSWORD"
@@ -15,7 +15,7 @@ su jboss -c "$JBOSS_HOME/bin/add-user.sh -s -a -u $MGMT_USER -g admin,guest -r A
 su jboss -c "$JBOSS_HOME/bin/add-user.sh -s -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p guest_12345!"
 
 /usr/local/bin/create-keystore.sh $JBOSS_BASE_DIR/configuration jboss12345 jboss.jboss
-su jboss -c "$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_BASE_DIR/../scripts/create-stomp-test.cli"
+su jboss -c "$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_BASE_DIR/scripts/create-stomp-test.cli"
 
 rm -rf $JBOSS_BASE_DIR/log/*.log $JBOSS_BASE_DIR/configuration/keystore.jks $JBOSS_BASE_DIR/configuration/standalone_xml_history/
 exit 0
--- src/main/docker/x1-wildfly-stomp-test/scripts/configure.sh
+++ src/main/docker/x1-wildfly-stomp-test/scripts/configure.sh
@@ -9,7 +9,7 @@ cd "$JBOSS_BASE_DIR"
 su jboss -c "$JBOSS_HOME/bin/add-user.sh -s -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p guest_12345!"
 
 /usr/local/bin/create-keystore.sh $JBOSS_BASE_DIR/configuration jboss12345 jboss.jboss
-su jboss -c "$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_BASE_DIR/../scripts/create-stomp-test.cli"
+su jboss -c "$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_BASE_DIR/scripts/create-stomp-test.cli"
 
 rm -rf $JBOSS_BASE_DIR/log/*.log $JBOSS_BASE_DIR/configuration/keystore.jks $JBOSS_BASE_DIR/configuration/standalone_xml_history/
 exit 0
