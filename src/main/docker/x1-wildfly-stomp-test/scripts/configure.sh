a0b9ce24cbf2dfaf4d54ca66b14b4c42d8344559
Johannes Beck <mail@johannes-beck.name>
2020-11-23 20:20:11 +0100

    Use chown in Dockerfile

--- src/main/docker/x1-wildfly-stomp-test/scripts/configure.sh
+++ src/main/docker/x1-wildfly-stomp-test/scripts/configure.sh
@@ -4,12 +4,11 @@ export JBOSS_BASE_DIR=$1
 export JBOSS_HOME=/opt/wildfly
 export JAVA_OPTS="-Djboss.server.base.dir=$JBOSS_BASE_DIR"
 
-chown -R jboss.jboss /srv/wildfly
 cd "$JBOSS_BASE_DIR"
-su jboss -c "$JBOSS_HOME/bin/add-user.sh -s -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p guest_12345!"
+$JBOSS_HOME/bin/add-user.sh -s -a -u guest -g guest -r ApplicationRealm -sc $JBOSS_BASE_DIR/configuration -p "guest_12345!"
 
 /usr/local/bin/create-keystore.sh $JBOSS_BASE_DIR/configuration jboss12345 jboss.jboss
-su jboss -c "$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_BASE_DIR/scripts/create-stomp-test.cli"
+$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_BASE_DIR/scripts/create-stomp-test.cli
 
 rm -rf $JBOSS_BASE_DIR/log/*.log $JBOSS_BASE_DIR/configuration/keystore.jks $JBOSS_BASE_DIR/configuration/standalone_xml_history/
 exit 0
