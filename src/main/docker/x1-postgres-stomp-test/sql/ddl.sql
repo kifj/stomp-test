9fa33ed79d476f5eebbfcf519769a148cbb19f92
Johannes Beck <mail@johannes-beck.name>
2022-11-30 11:45:29 +0100

    Use new EJB timer service

--- src/main/docker/x1-postgres-stomp-test/sql/ddl.sql
+++ src/main/docker/x1-postgres-stomp-test/sql/ddl.sql
@@ -12,33 +12,3 @@ CREATE TABLE stocks.share (
 
 CREATE UNIQUE INDEX idx_key ON stocks.share (key);
 CREATE INDEX idx_name ON stocks.share (name);
-
-CREATE TABLE stocks.jboss_ejb_timer (
-  id VARCHAR PRIMARY KEY NOT NULL,
-  timed_object_id VARCHAR NOT NULL,
-  initial_date TIMESTAMP,
-  repeat_interval BIGINT,
-  next_date TIMESTAMP,
-  previous_run TIMESTAMP,
-  primary_key VARCHAR,
-  info TEXT,
-  timer_state VARCHAR,
-  schedule_expr_second VARCHAR,
-  schedule_expr_minute VARCHAR,
-  schedule_expr_hour VARCHAR,
-  schedule_expr_day_of_week VARCHAR,
-  schedule_expr_day_of_month VARCHAR,
-  schedule_expr_month VARCHAR,
-  schedule_expr_year VARCHAR,
-  schedule_expr_start_date VARCHAR,
-  schedule_expr_end_date VARCHAR,
-  schedule_expr_timezone VARCHAR,
-  auto_timer BOOLEAN,
-  timeout_method_declaring_class VARCHAR,
-  timeout_method_name VARCHAR,
-  timeout_method_descriptor VARCHAR,
-  calendar_timer BOOLEAN,
-  partition_name VARCHAR NOT NULL,
-  node_name VARCHAR);
-
-CREATE INDEX jboss_ejb_timer_index ON stocks.jboss_ejb_timer (partition_name, timed_object_id);
