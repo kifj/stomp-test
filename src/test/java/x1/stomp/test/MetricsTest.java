2672a68d01d845f0a7187fa7021153cff3a6e41a
Johannes Beck <mail@johannes-beck.name>
2023-05-13 17:20:37 +0200

    First version of test with Micrometer

--- src/test/java/x1/stomp/test/MetricsTest.java
+++ src/test/java/x1/stomp/test/MetricsTest.java
@@ -4,7 +4,7 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.assertj.core.api.Assertions.assertThat;
 import static x1.stomp.test.ResponseAssert.assertThat;
 
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import jakarta.inject.Inject;
@@ -14,13 +14,11 @@ import static jakarta.ws.rs.core.Response.Status.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import com.google.gson.JsonParser;
 
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Tag;
 import x1.stomp.model.Share;
 
 @DisplayName("Metrics Test")
@@ -45,22 +43,6 @@ public class MetricsTest extends AbstractIT {
   @Inject
   private MeterRegistry registry; 
 
-  @ParameterizedTest
-  @ValueSource(strings = { "memory.committedHeap", "memory.committedNonHeap", "memory.maxHeap", "memory.maxNonHeap",
-      "memory.usedHeap", "memory.usedNonHeap" })
-  @DisplayName("test JVM memory metrics")
-  public void testMemoryMetrics(String key) {
-    var response = client.target(metricsBaseUrl).path("metrics").path("base").request(APPLICATION_JSON).get();
-    assertThat(response).hasStatus(OK);
-
-    var body = response.readEntity(String.class);
-    assertThat(body).isNotNull();
-
-    var o = JsonParser.parseString(body).getAsJsonObject();
-    assertThat(o).isNotNull();
-    assertThat(o.getAsJsonPrimitive(key).getAsLong()).isNotEqualTo(0);
-  }
-
   @Test
   @DisplayName("test health")
   public void testHealth() {
@@ -84,22 +66,8 @@ public class MetricsTest extends AbstractIT {
     });
     assertThat(shares).isEmpty();
 
-    var response = client.target(metricsBaseUrl).path("metrics").path("application").request(APPLICATION_JSON).get();
-    assertThat(response).hasStatus(OK);
-
-    var body = response.readEntity(String.class);
-    assertThat(body).isNotNull();
-
-    var o = JsonParser.parseString(body).getAsJsonObject();
-    assertThat(o).isNotNull();
-    assertThat(o.getAsJsonObject("add-share").getAsJsonPrimitive("count;interface=ShareResource").getAsInt()).isEqualTo(0);
-    assertThat(o.getAsJsonObject("get-share").getAsJsonPrimitive("count;interface=ShareResource").getAsInt()).isEqualTo(0);
-    assertThat(o.getAsJsonObject("remove-share").getAsJsonPrimitive("count;interface=ShareResource").getAsInt()).isEqualTo(0);
-    assertThat(o.getAsJsonObject("get-shares").getAsJsonPrimitive("count;interface=ShareResource").getAsInt()).isEqualTo(1);
-    
-    assertThat(registry.timer("add-share", Arrays.asList(Tag.of("interface","ShareResource"))).count()).isEqualTo(0l);
-    assertThat(registry.timer("get-share", Arrays.asList(Tag.of("interface","ShareResource"))).count()).isEqualTo(0l);
-    assertThat(registry.timer("remove-share", Arrays.asList(Tag.of("interface","ShareResource"))).count()).isEqualTo(0l);
-    assertThat(registry.timer("get-shares", Arrays.asList(Tag.of("interface","ShareResource"))).count()).isEqualTo(1l);
+    Collection<Counter> counters = registry.get("rest-request-status").counters();
+    assertThat(counters).isNotEmpty();
+    assertThat(counters).anyMatch(counter -> counter.count() > 0 && counter.getId().getTag("method").equals("listAllShares"));
   }
 }
