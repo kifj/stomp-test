a34e0780b4b51a2a8494db2cd7269f2d8ae86e50
Johannes Beck <mail@johannes-beck.name>
2023-11-04 13:40:30 +0100

    More flexible configuration

--- src/main/webapp/swagger/swagger-initializer.js
+++ src/main/webapp/swagger/swagger-initializer.js
@@ -4,13 +4,9 @@ window.onload = function() {
   if (url && url.length > 1) {
     url = decodeURIComponent(url[1]);
   } else {
-    var path = '';
-    if (window.location.pathname.length > 1) {
-      path = '/' + window.location.pathname.split( '/' )[1];
-    }
-    url = window.location.protocol + "//" + location.host + path + "/openapi?format=JSON";
+    url = window.location.protocol + "//" + location.host +  "/openapi?format=JSON";
   }
-  
+
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: url,
