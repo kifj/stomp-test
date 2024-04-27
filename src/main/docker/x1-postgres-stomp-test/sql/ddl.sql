b3a7dc8d7d01f8fe55e7c2c05cf43083fca78754
Johannes Beck <mail@johannes-beck.name>
2024-04-27 22:07:42 +0200

    Fix DDL script

--- src/main/docker/x1-postgres-stomp-test/sql/ddl.sql
+++ src/main/docker/x1-postgres-stomp-test/sql/ddl.sql
@@ -6,7 +6,7 @@ CREATE TABLE stocks.share (
   name character varying(80) NOT NULL,
   version bigint,
   CONSTRAINT share_pkey PRIMARY KEY (id),
-  CONSTRAINT idx_key UNIQUE (key);
+  CONSTRAINT idx_key UNIQUE (key)
 );
 
 CREATE SEQUENCE stocks.share_seq INCREMENT BY 50;
