50f3854cd73e7d96034d8e848eb4e21560b32dab
Johannes Beck <mail@johannes-beck.name>
2025-10-19 15:00:12 +0200

    Update to Wildfly 38

--- src/test/java/x1/arquillian/Wildfly.java
+++ src/test/java/x1/arquillian/Wildfly.java
@@ -12,5 +12,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.PARAMETER})
 public @interface Wildfly {
-    String image() default "registry.x1/j7beck/x1-wildfly-profile:37.0.1.Final";
+    String image() default "registry.x1/j7beck/x1-wildfly-profile:38.0.0.Final";
 }
